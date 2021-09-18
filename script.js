91627f82227993c75882ac369dabd1826acaf9d5
RonnyMV <rjmv.dev@gmail.com>
2021-09-18 20:54:41 -0300

    requisito 3 e 4

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+// requisito 1
 const createTitle = document.createElement('header');
 createTitle.innerText = 'Minha Lista de Tarefas';
 document.body.appendChild(createTitle);
 const createParagraph = document.createElement('p');
-
+// requisito 2
 createParagraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
 createParagraph.id = 'funcionamento';
 document.body.appendChild(createParagraph);
+
+// requisito 3
+const CreateInput = document.createElement('input');
+CreateInput.id = 'texto-tarefa';
+document.body.appendChild(CreateInput);
+
+// requisito 4
+const createOrderedList = document.createElement('ol')
+createOrderedList.id = 'lista-tarefas';
+document.body.appendChild(createOrderedList);
