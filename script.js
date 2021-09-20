34d9280df3edaec1c5d577ecc92c93d3c4f70b52
Joey <joviavelino@gmail.com>
2021-09-20 02:22:47 -0300

    requisitos 5 e 6

---
+++ script.js
@@ -0,0 +1,13 @@
+const listaDeTarefa = document.getElementById("lista-tarefas")
+const buttonTask =  document.getElementById("criar-tarefa")
+const inputText = document.getElementById("texto-tarefa")
+
+
+function createTask(){
+    const itemListaDeTarefas = document.createElement("li");
+    listaDeTarefa.appendChild(itemListaDeTarefas);
+    itemListaDeTarefas.innerText = inputText.value;
+    inputText.value = " ";
+}
+
+buttonTask.addEventListener("click",createTask)
