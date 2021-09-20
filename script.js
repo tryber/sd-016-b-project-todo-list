0b5fd61b63f14d7a4da731746e7e57faebc4b2a9
Millene Galetti <mikagaletti1@gmail.com>
2021-09-20 11:25:41 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-const button = document.querySelector("#criar-tarefa");
+const taskButton = document.querySelector("#criar-tarefa");
 const listOfTasks = document.querySelector('#lista-tarefas');
-const input = document.querySelector('#texto-tarefa');
+const taskInput = document.querySelector('#texto-tarefa');
 
-function createTask () {
+// Requisito 5
+// Fonte de pesquisa : https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/this
+taskInput.addEventListener('input', function(event) {
+    event.target.value = this.value;
+})
 
-}
+// fonte de pesquisa : https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+taskButton.addEventListener('click',() => {
+  const task =document.createElement('li');
+  task.innerHTML = taskInput.nodeValue;
+  task.classList.add('tarefa-criada');
+  listOfTasks.appendChild(task);
+  taskInput.value = '';
+});
 
-function addTasksInList(class) {
-    const list = document.createElement('li');
-    list.className = class;
-    listOfTasks.appendChild(list);
-}
+listOfTasks.addEventListener('click', function (event) {
+    const selectTask = event.target;
+    if(previousSelected.length > 0){
+        previousSelected[0].classList.remove("selected");
+            if(selectedTask.classList.contains("selected")){
+            selectedTask.classList.remove("selected");
+        } else{
+            selectedTask.classList.add("selected");
+        }
+    }
+    else{
+        selectedTask.classList.add("selected");
+    }
+})
 
-button.addEventListener('click',createTask);
+// Requisito 9
+listOfTasks.addEventListener("dblclick", function(event){
+    let selectedTask = event.target;
+    if(selectedTask.classList.contains("completed")){
+        selectedTask.classList.remove("completed");
+    } else{
+        selectedTask.classList.add("completed");
+    }
+})
