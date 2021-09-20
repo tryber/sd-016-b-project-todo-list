0a204bff232347ec8fe9118b1c0c79d6aaa81bdb
Guilherme Rosa <gui.rsa@hotmail.com>
2021-09-20 09:58:21 -0300

    mudancas 7 e 8

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
 console.log('vamo q vamo');
+const orderedList = document.getElementById('lista-tarefas');
 function createListItem() {
   const valorInput = document.getElementById('texto-tarefa');
-  const orderedList = document.getElementById('lista-tarefas');
   const p = document.createElement('li');
   p.classList.add('lista');
   orderedList.appendChild(p).innerText = valorInput.value;
   valorInput.value = '';
+
+  const getListelements = document.getElementsByClassName('lista');
+  for (let index = 0; index < getListelements.length; index += 1) {
+    getListelements[index].addEventListener('click', changeBackgroundColorToGray);
+  }
 }
 
 const submitButton = document.getElementById('criar-tarefa');
 submitButton.addEventListener('click', createListItem);
 
 // mudando para cor cinza
-
-function changeBackgroundColorToGray(Event) {
-  Event.target.style.backgroundColor = 'red';
+function changeBackgroundColorToGray(event) {
+  const seletor = document.getElementsByTagName('li');
+  for (let index = 0; index < seletor.length; index += 1) {
+    seletor[index].classList.add('selected');
+    seletor[index].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
 }
 
-const getListelements = document.getElementById('lista-tarefas').children;
+const getListelements = document.getElementsByClassName('lista');
 for (let index = 0; index < getListelements.length; index += 1) {
   getListelements[index].addEventListener('click', changeBackgroundColorToGray);
 }
 
 // apagar tudo
 function eraseAll() {
-  const orderedList = document.getElementById('lista-tarefas');
   orderedList.innerHTML = '';
 }
 
-let eraseButton = document.getElementById('apaga-tudo');
+const eraseButton = document.getElementById('apaga-tudo');
 eraseButton.addEventListener('click', eraseAll);
