14138ea2be8a9f663db121eeb49b6cbc14fa233e
AmandaSottani <amandasottani@gmail.com>
2021-09-20 17:47:24 -0300

    requisito 7 e 8

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 window.onload = function beginning() {
-
   // função que faz o botao incluir uma tarefa no fim da lista
-  let btn =document.getElementById("criar-tarefa").addEventListener("click",createTask);
-  let task = document.getElementById("texto-tarefa");
+  
+  document.getElementById('criar-tarefa').addEventListener('click',createTask);
+  const task = document.getElementById('texto-tarefa');
 
-  function createTask(event) {  
-    list = document.getElementById("lista-tarefas");
-    let listItem = document.createElement("li");
+  function createTask() {  
+    const list = document.getElementById('lista-tarefas');
+    const listItem = document.createElement('li');
     listItem.innerText = task.value;
     list.appendChild(listItem);
+    listItem.classList.add('item');
     task.value = '';
+  }
+ 
+  const taskList = document.getElementById('lista-tarefas');taskList.addEventListener('click', seleciona);
+
+  function seleciona(event){
+    const items = document.querySelectorAll('li');
+    for (let i = 0; i < items.length; i ++) {
+      items[i].style.backgroundColor = '';
+      items[i].classList.remove('selected');
+      event.target.classList.add('selected');
+    }
   }
 }
