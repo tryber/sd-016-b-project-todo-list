369221f79cdff0078dafccf583d65943f534b247
AmandaSottani <amandasottani@gmail.com>
2021-09-20 18:41:30 -0300

    requisito 9

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-window.onload = function beginning() {
-  // função que faz o botao incluir uma tarefa no fim da lista
-  
-  document.getElementById('criar-tarefa').addEventListener('click',createTask);
-  const task = document.getElementById('texto-tarefa');
+document.getElementById('criar-tarefa').addEventListener('click', createTask);
+const task = document.getElementById('texto-tarefa');
 
-  function createTask() {  
-    const list = document.getElementById('lista-tarefas');
-    const listItem = document.createElement('li');
-    listItem.innerText = task.value;
-    list.appendChild(listItem);
-    listItem.classList.add('item');
-    task.value = '';
-  }
+const taskList = document.getElementById('lista-tarefas');taskList.addEventListener('click', seleciona);
+taskList.addEventListener('dblclick', complete);
  
-  const taskList = document.getElementById('lista-tarefas');taskList.addEventListener('click', seleciona);
-
-  function seleciona(event){
-    const items = document.querySelectorAll('li');
-    for (let i = 0; i < items.length; i ++) {
-      items[i].style.backgroundColor = '';
-      items[i].classList.remove('selected');
-      event.target.classList.add('selected');
-    }
+// função que faz o botao incluir uma tarefa no fim da lista
+function createTask() {  
+  const list = document.getElementById('lista-tarefas');
+  const listItem = document.createElement('li');
+  listItem.innerText = task.value;
+  list.appendChild(listItem);
+  listItem.classList.add('item');
+  task.value = '';
+}
+//função que muda a cor da task selecionada
+function seleciona(event) {
+  const items = document.querySelectorAll('li');
+  for (let i = 0; i < items.length; i += 1 ) {
+    items[i].style.backgroundColor = '';
+    items[i].classList.remove('selected');
+    event.target.classList.add('selected');
+  }
+}
+//função que risca task completa
+function complete (event) {
+  if(event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
   }
 }
