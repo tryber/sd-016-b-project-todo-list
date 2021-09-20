8afcae4f22160eebf3467018e3e9a26ed7d5de68
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2021-09-19 23:11:37 -0300

    Adequação ao Lint:styles e ajustes no scrip.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Para a parte de JS, foram consultados os materiais de W3Schools, MDN Web Docs, 
-// Course da Trybe, e aulas da Seção 7 "Working with the DOM (Browser HTML Code) in 
+// Para a parte de JS, foram consultados os materiais de W3Schools, MDN Web Docs,
+// Course da Trybe, e aulas da Seção 7 "Working with the DOM (Browser HTML Code) in
 // JavaScript" do curso "JavaScript - The Complete Guide 2021 (Beginner + Advanced)",
-// ministrado por Maximilian Schwarzmüller. 
+// ministrado por Maximilian Schwarzmüller.
 
 // Links:
 // https://www.w3schools.com/
@@ -9,38 +9,25 @@
 // https://app.betrybe.com/course
 // https://www.udemy.com/course/javascript-the-complete-guide-2020-beginner-advanced/
 
-window.onload = function () {
-
-  const addTaskButton = document.querySelector('#criar-tarefa');
-  addTaskButton.addEventListener('click', addTask); // Quando o botao é clicado, a função addTask é chamada.
-
-  const removeAllButton = document.querySelector('#apaga-tudo');
-  removeAllButton.addEventListener('click', removeAllTasks); // Quando o botao é clicado, a função removeAllTasks é chamada.
-
-  const removeCompletedTaskButton = document.querySelector('#remover-finalizados');
-  removeCompletedTaskButton.addEventListener('click', removeCompletedTasks); // Quando o botao é clicado, a função removeCompletedTasks é chamada.
-
-}
-
 function addTask() {
-  //Adiciona um item na lista de tarefas que interage com cliques do usuário.
+  // Adiciona um item na lista de tarefas que interage com cliques do usuário.
 
   const taskText = document.querySelector('#texto-tarefa');
   const toDoList = document.querySelector('#lista-tarefas');
 
-  if (taskText.value !== "") {
-    const task = document.createElement('li'); //Cria um elemento li e o atribui na constante task.
-    task.setAttribute('class', 'task'); //Configura o atributo class="task" a li criada.
+  if (taskText.value !== '') {
+    const task = document.createElement('li'); // Cria um elemento li e o atribui na constante task.
+    task.setAttribute('class', 'task'); // Configura o atributo class="task" a li criada.
 
-    task.innerText = taskText.value; //Substitui o texto visível da li criada pelo valor do input #texto-tarefa.
-    toDoList.appendChild(task); //Adiciona a li criada já com o texto do input a lista ordenada #lista-tarefa.
-    taskText.value = ""; //Substitui o valor do input para string vazia, ou seja, remove o valor do input.
+    task.innerText = taskText.value; // Substitui o texto visível da li criada pelo valor do input #texto-tarefa.
+    toDoList.appendChild(task); // Adiciona a li criada já com o texto do input a lista ordenada #lista-tarefa.
+    taskText.value = ''; // Substitui o valor do input para string vazia, ou seja, remove o valor do input.
 
     // Chama a funçao riskTask a partir do evento clique duplo.
     task.addEventListener('dblclick', function riskText() {
       // Risca o texto da li.
       task.classList.toggle('completed');
-      //Por meio do classList é possível adicionar ou retirar uma classe ao elemento. O método toggle permite que adicione  atribute class="completed" quando o retorno é false, ou seja, quando essa classe não é um atributo do elemento, e retira esse atributo quando o retorno é true.
+      // Por meio do classList é possível adicionar ou retirar uma classe ao elemento. O método toggle permite que adicione  atribute class="completed" quando o retorno é false, ou seja, quando essa classe não é um atributo do elemento, e retira esse atributo quando o retorno é true.
     });
 
     // Chama a função changeBrackground a partir do evento clique.
@@ -70,3 +57,14 @@ function removeCompletedTasks() {
     completedTasks[0].parentNode.removeChild(completedTasks[0]);
   }
 }
+
+window.onload = function () {
+  const addTaskButton = document.querySelector('#criar-tarefa');
+  addTaskButton.addEventListener('click', addTask); // Quando o botao é clicado, a função addTask é chamada.
+
+  const removeAllButton = document.querySelector('#apaga-tudo');
+  removeAllButton.addEventListener('click', removeAllTasks); // Quando o botao é clicado, a função removeAllTasks é chamada.
+
+  const removeCompletedTaskButton = document.querySelector('#remover-finalizados');
+  removeCompletedTaskButton.addEventListener('click', removeCompletedTasks); // Quando o botao é clicado, a função removeCompletedTasks é chamada.
+};
