7df5d3004b423d897f5bece6ce4341038e544d8a
OiEuSouOPi <rafasc866@gmail.com>
2021-09-20 22:55:28 -0300

    o nome que eu quiser

--- script.js
+++ script.js
@@ -10,19 +10,35 @@ let lista = document.createElement("li");
 lista.setAttribute("id",contador);
 lista.setAttribute("class","list");
 contador+=1;
+lista.onclick =new Function("selecionadora(this)")
+lista.ondblclick = new Function("DBclick(this)")
 lista.innerText = criarLista;
 salvando.push(criarLista)
 ordenada.append(lista);
 tarefa.value = ""; 
+
 }
-function selecionadora(){
+function selecionadora(a){
   let adicionarClasse = document.getElementsByClassName("list")
   for(let i=0; i<salvando.length; i+=1){
-    adicionarClasse[i].addEventListener("click",pintar);
+    adicionarClasse[i].style = "background:rgb(255, 255, 255)";
+  if (document.getElementById(a.id).style != null) {
+    document.getElementById(a.id).style = "background:rgb(128, 128, 128)";
+  }
+    else{
+      document.getElementById(a.id).style = "background:rgb(255, 255, 255)"
+    }
   }
 }
-function pintar(event){
-    let cinza = event.target.classList;
-    cinza.toggle()
-  }
   selecionadora()
+function DBclick(b){
+  let incompleto = document.getElementsByClassName("list")
+  for(let i=0; o<salvando.length; i+=1){
+    incompleto[i].classList.add("incompleto")
+  if (b.target.classList.contains('completed') === true) {
+   b.target.classList.remove('completed');
+  } else {
+    b.target.classList.add('completed');
+  }
+}
+DBclick()
