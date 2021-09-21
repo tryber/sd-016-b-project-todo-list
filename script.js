19451d3c4c89b0733c25ba685efc2f6d617d471a
Helder Emanuel Alves de Oliveira <hlremanuel16@gmail.com>
2021-09-21 11:43:59 -0300

    resolve os requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-// requisito 5-6  
-const butt = document.getElementById('criar-tarefa');
-
-function createLi() {
+window.onload = function letsBegin(){
+  // requisito 5-6  
+  const butt = document.getElementById('criar-tarefa');
   const list = document.querySelector('#lista-tarefas');
-  let li = document.createElement('li');
-  li.classList.add('listItem');
-  li.innerText = document.getElementById('texto-tarefa').value;
-  list.appendChild(li);
-  document.getElementById('texto-tarefa').value = '';
+  
+  function createLi() {
+    let li = document.createElement('li');
+    li.classList.add('listItem');
+    li.innerText = document.getElementById('texto-tarefa').value;
+    list.appendChild(li);
+    document.getElementById('texto-tarefa').value = '';
+  }
+  
+  butt.addEventListener('click', createLi);
+  
+//REQUISITO 7
+
+  list.addEventListener('click', liColorChange)
+
+  const listItem = document.getElementsByClassName('listItem')
+  function liColorChange() {
+    for (i = 0; i< listItem.length; i += 1) {
+      listItem[i].classList.remove('bckGrey')
+    }
+  }
+  list.addEventListener('click', liColorChange2)
+  function liColorChange2(event) {
+    event.target.classList.add('bckGrey')
+  }
+
 }
 
-butt.addEventListener('click', createLi);
 
   
