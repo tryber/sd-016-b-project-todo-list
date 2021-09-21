0203749975f64efb91b5dbe87b08f21b7b1cf879
Rafael Santos Carvalho <88905368+Oieusouopi@users.noreply.github.com>
2021-09-21 14:04:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-  let botaoApag = document.getElementById("apaga-tudo")
+let botaoApag = document.getElementById("apaga-tudo")
 let ordenada = document.getElementById("lista-tarefas");
 let tarefa = document.getElementById("texto-tarefa");
 let botao = document.getElementById("criar-tarefa");
@@ -12,30 +12,31 @@ let lista = document.createElement("li");
 lista.setAttribute("id",contador);
 lista.setAttribute("class","list");
 contador+=1;
-lista.onclick =new Function("selecionadora(this)")
+lista.addEventListener('click',selecionadora)
 lista.innerText = criarLista;
 salvando.push(criarLista)
 ordenada.append(lista);
 tarefa.value = ""; 
 
 }
-function selecionadora(a){
+function selecionadora(event){
   let adicionarClasse = document.getElementsByClassName("list")
   for(let i=0; i<salvando.length; i+=1){
     adicionarClasse[i].style = "background:rgb(255, 255, 255)";
-  if (document.getElementById(a.id).style != null) {
-    document.getElementById(a.id).style = "background:rgb(128, 128, 128)";
+  if (event.target.style != null) {
+    event.target.style = "background:rgb(128, 128, 128)";
   }
     else{
-      document.getElementById(a.id).style = "background:rgb(255, 255, 255)"
+      event.target.style = "background:rgb(255, 255, 255)"
     }
   }
 }
+selecionadora()
   botaoApag.addEventListener("click",Clean)
 function Clean(){
     for (let index = 0; index < ordenada.children.length; index = 0) {
       ordenada.children[index].remove();
     }
 }
-  selecionadora()
+
 }
