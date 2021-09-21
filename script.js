c40cde55a57e9c9715439b63308a99539dda102d
Joey <joviavelino@gmail.com>
2021-09-21 01:50:55 -0300

    requisitos 9 e 10

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const listaDeTarefa = document.getElementById("lista-tarefas")
 const buttonTask =  document.getElementById("criar-tarefa")
 const inputText = document.getElementById("texto-tarefa")
-
+const deletButton = document.getElementById("apaga-tudo")
 
 function createTask(){
     const itemListaDeTarefas = document.createElement("li");
@@ -16,11 +16,30 @@ buttonTask.addEventListener("click",createTask)
 function paintTask (event){
     const selectedPaint = document.querySelector("paint")
     if(selectedPaint === null) {
-        event.target.classList.add("paint")
+        event.target.classList.add("paint");
     } else {
-    selectedPaint.classList.remove("paint")
-    event.target.classlist.add("paint")
+        selectedPaint.classList.remove("paint");
+        event.target.classList.add("paint");
     }
 }
 
 listaDeTarefa.addEventListener("click", paintTask)
+
+
+
+function completeTask (event){
+    if(event.target.classList.contains("completed")) {
+        event.target.classList.remove("completed");
+    } else {
+        event.target.classList.add("completed");
+    }
+}
+    
+listaDeTarefa.addEventListener("dblclick", completeTask)
+
+
+function deletAll () {
+listaDeTarefa.innerText = " "
+}
+
+deletButton.addEventListener("click",deletAll)
