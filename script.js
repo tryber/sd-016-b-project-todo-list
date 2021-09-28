10581f169948a90abda11d1c603244bc0fc99687
Murilo Costa <mcosta12358@gmail.com>
2021-09-28 11:48:03 -0300

    comitao bomba

--- script.js
+++ script.js
@@ -1,53 +1,26 @@
-let submit = document.getElementById('criar-tarefa');
-let textField = document.getElementById('texto-tarefa');
-let listElements = document.getElementsByTagName('li');
-let valueInput = '';
+window.onload = function(){
+    
+    getValueAndSubmit();
+    
+}
 
-    textField.addEventListener('blur',function(){
-        valueInput = document.getElementById('texto-tarefa').value;    
-        console.log(valueInput);
-    })
 
+function getValueAndSubmit(){
+    
+    let submit = document.getElementById('criar-tarefa');
+    let taskList = document.getElementById('lista-tarefas');
+    let inputText = document.getElementById('texto-tarefa');
+        
     submit.addEventListener('click',function(){
 
-        let li = document.createElement('li');
-        let taskList = document.getElementById('lista-tarefas');
-        let taskListLength = taskList.children.length;
-        taskList.appendChild(li);
-        li.innerText = valueInput;
-        textField.value = ''; 
-
-        if (taskList.children != 0 ){
-             let great = taskListLength;
-            for (let index = 0; index < taskListLength; index++) {
-                               
-                taskList.children[great]= taskList.children[index];
-                great--;
-              
-            }
-
-            
-        }
-
-        for (let index = 0; index < listElements.length; index++) {
-            
-            listElements[index].addEventListener('click',function(event){
-
-                let position = Array.from(listElements).indexOf(event.target);
-
-                event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-
-                for (let index = 0; index <  listElements.length; index++) {
-                    
-                    if (listElements[index] != listElements[position]){
-                        listElements[index].style.backgroundColor = '';     
-                    }
-                }
-        })
-    }       
-    })
-
-
-      
+        let itens = document.createElement('li');
+           
+        itens.innerText = inputText.value;
         
-     
+        taskList.appendChild(itens);
+
+        inputText.value = '';
+       
+        })
+    
+}
