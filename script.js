e7e6bad264dfe81228da2d7c7dbec4b079a0ecdf
Murilo Costa <mcosta12358@gmail.com>
2021-10-07 00:08:57 -0300

    Implementando o requisito 9

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-window.onload = function () {
-    
-    getValueAndSubmit();
-   
-}
-
+getValueAndSubmit();
 
 function getValueAndSubmit(){
     
     let submit = document.getElementById('criar-tarefa');
     let taskList = document.getElementById('lista-tarefas');
     let inputText = document.getElementById('texto-tarefa');
-        
+    let itensLista = document.getElementsByTagName('li');
+    
+    
     submit.addEventListener('click',function(){
 
         let itens = document.createElement('li');
@@ -20,36 +17,40 @@ function getValueAndSubmit(){
         taskList.appendChild(itens);
 
         inputText.value = '';
-       
-        setBGcolor ();
 
-        })
+        
+
+        for (let index = 0; index < itensLista.length; index++) {
+       
+            itensLista[index].addEventListener('click',setBGcolor);
+            itensLista[index].addEventListener('dblclick',setLineThrough);
+                
+       }
+
+       })
     
 }
 
-function setBGcolor (){
-
-    let li = document.getElementsByTagName('li');
-   
-
-    for (let index = 0; index < li.length; index++) {
+function setBGcolor(event){
         
-        li[index].addEventListener('click',function(event){
-
-            let position = Array.from(li).indexOf(event.target);
+        let itensLista = document.getElementsByTagName('li')
+        
+        let position = Array.from(itensLista).indexOf(event.target);
             
-            li[position].style.backgroundColor = 'rgb(128,128,128)';
+        itensLista[position].style.backgroundColor = 'rgb(128,128,128)';
            
-            for (let index = 0; index < li.length; index++) {
+        for (let index = 0; index < itensLista.length; index++) {
                 
                 if (index != position){
                    
-                    li[index].style.backgroundColor = '';
+                    itensLista[index].style.backgroundColor = '';
                 }
                 
-            }
-        })
-        
-    }
+        }
+}   
 
+function setLineThrough (event){
+
+            event.target.classList.toggle('completed');
+  
 }
