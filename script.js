70d3fe3d140edcf54db3c1913a81e3e71e81f8f7
Borba <luisfpb@icloud.com>
2021-10-10 21:49:40 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -3,51 +3,41 @@ const taskList = document.getElementById('lista-tarefas');
 const inputField = document.getElementById('texto-tarefa');
 const resetButton = document.getElementById('apaga-tudo');
 const removeButton = document.getElementById('remover-finalizados');
-const saveTasksButton = document.getElementById("salvar-tarefas")
+const saveTasksButton = document.getElementById("salvar-tarefas");
 
 function removeCompletedTask() {
   Array.from(taskList.children).forEach((task) => {
-    if (task.classList.contains('completed')) task.remove();
+    if (task.classList.contains('completed')) task.remove()
   });
+};
+
+function backgroundColor(event) {
+  event.target.style.backgroundColor = 'gray';
+};
+
+function lineThrough(e) {
+  if (e.target.className === 'completed') {
+    e.target.className = '';
+  } else {
+    e.target.className = 'completed';
+  }
 }
 
-function saveTasks() {
-  window.localStorage.setItem('tasks', JSON.stringify(taskList.innerHTML));
-}
-
-function getTasks() {
-  const tasks = JSON.parse(window.localStorage.getItem('tasks'));
-  taskList.innerHTML = tasks;
-  Array.from(taskList.children).forEach((task) => {
-    addTaskEventListeners(task);
-  });
-}
-
-function switchCompletedTask(event) {
-  const taskSelected = event.target;
-  taskSelected.classList.toggle('completed');
-}
-
-addButton.addEventListener ('click', function() {
+addButton.addEventListener('click', function() {
 
   const addList = document.createElement('li');
   addList.innerText = inputField.value;
   inputField.value = '';
   taskList.appendChild(addList);
 
-  addList.addEventListener('click', function() {
-    addList.classList.add('selected');
-  });
+  addList.addEventListener('click', backgroundColor)
 
-  addList.addEventListener('dblclick', switchCompletedTask);
-
-});
+  addList.addEventListener('dblclick', lineThrough)
 
 resetButton.addEventListener('click', function() {
   taskList.innerHTML = ''
 });
 
-removeButton.addEventListener('click', removeCompletedTask);
+});
 
-saveTasksButton.addEventListener('click', saveTasks);
-  getTasks();
+removeButton.addEventListener('click', removeCompletedTask);
